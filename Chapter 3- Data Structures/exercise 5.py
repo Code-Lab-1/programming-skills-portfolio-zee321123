1164f1677bcdf72c9d209d8a1856e11df464080c
zaid <zaidbathspa.ae@gmail.com>
2022-11-04 19:28:57 +0400

    Create exercise 5.py

---
+++ Chapter 3- Data Structures/exercise 5.py
@@ -0,0 +1,27 @@
+guests = ['jadon', 'zaid', 'usman']
+
+name = guests[0].title()
+print(name + ", please come to dinner.")
+
+name = guests[1].title()
+print(name + ", please come to dinner.")
+
+name = guests[2].title()
+print(name + ", please come to dinner.")
+
+name = guests[1].title()
+print("\nSorry, " + name + " can't make it to dinner.")
+
+# zaid can't make it! Let's invite iftikhar instead.
+del(guests[1])
+guests.insert(1, 'iftikhar')
+
+# Print the invitations again.
+name = guests[0].title()
+print("\n" + name + ", please come to dinner.")
+
+name = guests[1].title()
+print(name + ", please come to dinner.")
+
+name = guests[2].title()
+print(name + ", please come to dinner.")
