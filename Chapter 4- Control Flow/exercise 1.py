c7296a9d5da6257113cd6e13ece2731f37a19ad7
zaid <zaidbathspa.ae@gmail.com>
2022-11-24 17:11:32 +0400

    Update exercise 1.py

--- Chapter 4- Control Flow/exercise 1.py
+++ Chapter 4- Control Flow/exercise 1.py
@@ -1,9 +1,10 @@
+#passing version
 alien_color = 'green'
 
 if alien_color == 'green':
     print("You just earned 5 points!")
 
-
+#failing version
     alien_color = 'red'
 
 if alien_color == 'green':
